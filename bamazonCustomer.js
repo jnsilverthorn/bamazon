5baad914d5062a374ef4a25a0b19525a40703ecd
J. Silverthorn <jnsilverthorn@ucdavis.edu>
2019-12-04 19:26:56 -0800

    added query to db for data, formatted it in terminal, and added an endConnection function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,11 +4,39 @@ var inquirer = require('inquirer');
 var con = mySQL.createConnection({
     host: "localhost",
     user: "root",
-    password: "root"
+    password: "root",
+    database: "bamazon"
 });
 
-con.connect(function (error) {
-    if (error) throw error;
-    console.log("Connected!")
+con.connect((err) => {
+    if (err) throw err;
+    console.log("Connected.")
+    console.log("----------------------\n");
 });
 
+con.query('select * from bamazon.products', (err, rows) => {
+    if (err) throw err;
+
+    //console.log('Data received from Db:\n');
+    //console.log(rows);
+
+    rows.forEach((row) => {
+        console.log(`Product: ${row.product_name}`);
+        console.log(`Product ID: ${row.item_id}\n`)
+        console.log(`Price: $${row.price}.00`);
+        console.log(`In Stock: ${row.stock_quantity}\n`);
+        console.log("----------------------\n");
+    });
+});
+
+
+
+
+function endConnection() {
+    con.end((err) => {
+        if (err) throw err;
+        console.log("Connection Terminated.")
+    });
+}
+
+endConnection();
